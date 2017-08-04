33fa4a8c644720749e16c96ad77c008e1ec1b99c
silence <617592793@qq.com>
2017-08-04 23:34:30 +0800

    后台添加注册登录判断

--- apps/index/controller/Login.php
+++ apps/index/controller/Login.php
@@ -5,6 +5,7 @@ use think\Controller;
 use think\request;
 use think\Url;
 use think\Db;
+use think\Session;
 
 class Login extends Controller
 {
@@ -14,9 +15,70 @@ class Login extends Controller
 	 */
 	 public function login()
 	{
-		$username = input('get.username');
-		$password = md5(input('get.password'));
+		$username = input('post.username');
+		$password = md5(input('post.password'));
 
-		echo $password;
+		if(!$username || !$password){
+			$this->error('请求错误','Index/index');
+			return;
+		}
+
+		$result = Db::name('users')->where('username', $username)->find(); //检测用户名
+		if(!$result)
+			$result = Db::name('users')->where('email', $username)->find(); //检测邮箱
+		if(!$result){
+			echo '{"success":false,"msg":"用户名或邮箱不存在"}';
+			return;
+		}
+
+		$username = $result['username'];
+
+		if($password != $result['password']){
+			echo '{"success":false,"msg":"密码错误"}';
+			return;
+		}else{
+			$nowtime=date("Y-m-d H:i:s"); //此次登录时间
+			$update = Db::name('users')->where('username', $username)->update(['last_login_time'=>$nowtime]); //更新最后一次登录时间
+			Session::set('username', $username);
+			echo '{"success":true,"msg":"登录成功"}';
+		}
+	}
+
+	/**
+	 * 用户注册
+	 * @return json
+	 */
+	public function register()
+	{
+		$username  = input('get.username');
+		$password  = md5(input('get.password'));
+		$password2 = md5(input('get.password2'));
+		$email     = input('get.email');
+
+		if(!$username || !$password || !$password2){
+			$this->error('请求错误','Index/index');
+			return;
+		}
+
+		$check = Db::name('users')->where('username', $username)->find();
+		$check_email = Db::name('users')->where('email', $email)->find();
+		if($check){
+			echo '{"success":false,"msg":"用户名已存在"}';
+			return;
+		}
+		if($check_email){
+			echo '{}';
+		}
+		if($password != $password2){
+			echo '{"success":false,"msg":"两次输入的密码不一致"}';
+			return;
+		}
+
+		$nowtime = date("Y-m-d H:i:s"); //此次登录时间 & 创建时间
+		$insert = Db::name('users')->insert(['username'=>$username, 'password'=>$password, 'email'=>$email, 'last_login_time'=>$nowtime, 'create_time'=>$nowtime]); // 将数据插入数据表
+		Session::set('username', $username);
+
+		echo '{"success":true,"msg":"注册成功"}';
+
 	}
 }
